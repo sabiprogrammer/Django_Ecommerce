123c909d0587de1eb7ed204c581c4c58b7b56177
sabiprogrammer <obedisonline@gmail.com>
2020-12-22 01:19:46 +0100

    staaticfilddjjj

--- Dennis/settings.py
+++ Dennis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 # import dj_database_url
 # from decouple import config
 
@@ -152,4 +152,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
